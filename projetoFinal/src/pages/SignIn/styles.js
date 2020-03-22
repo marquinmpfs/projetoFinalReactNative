c41ed7910075cceb46bd525bd05960dfffe2762c
Marcos Paulo Ferreira da Silva <marquinmpfs@gmail.com>
2020-03-22 19:17:23 -0300

    Styling loggin page

--- projetoFinal/src/pages/SignIn/styles.js
+++ projetoFinal/src/pages/SignIn/styles.js
@@ -1,19 +1,56 @@
-import styled from 'styled-components';
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
 
-export const Background = styled.View``;
+export const Background = styled(LinearGradient).attrs({
+    colors: ['#262630', '#1b2d4e']
+})`
+    flex: 1;
+`;
 
-export const Container = styled.View``;
+export const Container = styled.KeyboardAvoidingView`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
 
-export const Logo = styled.View``;
+export const Logo = styled.Image`
+    margin-bottom: 15px;
+`;
 
-export const AreaInput = styled.View``;
+export const AreaInput = styled.View`
+    flex-direction: row;
+`;
 
-export const Input = styled.View``;
+export const Input = styled.TextInput`
+    background: #FFF;
+    color: #222;
+    font-size: 17px;
+    border-radius: 7px;
+    width: 90%;
+    margin-bottom: 15px;
+    padding: 10px;
+`;
 
-export const SubmitButton = styled.View``;
+export const SubmitButton = styled.TouchableOpacity`
+    align-items: center;
+    justify-content: center;
+    background-color: #35AAFF;
+    height: 45px;
+    width: 90%;
+    border-radius: 7px;
+    margin-top: 10px;
+`;
 
-export const SubmitText = styled.View``;
+export const SubmitText = styled.Text`
+    color: #FFF;
+    font-size: 18px;
+`;
 
-export const SignUpLink = styled.View``;
+export const SignUpLink = styled.TouchableOpacity`
+    margin-top: 10px;
+`;
 
-export const SignUpText = styled.View``;
+export const SignUpText = styled.Text`
+    color: #FFF;
+    padding-bottom: 13px;
+`;
