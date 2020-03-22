a9fdc274b2d8a7062d869dcae25e93b4352118ed
Marcos Paulo Ferreira da Silva <marquinmpfs@gmail.com>
2020-03-22 20:01:04 -0300

    Add firebase and config

---
+++ projetoFinal/src/services/fibaseconnection.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+let config = {
+    apiKey: "AIzaSyB8Yfn0G7vMIIr1A1wiSJpZigHxZp72yoY",
+    authDomain: "meudinheiro-1bfcb.firebaseapp.com",
+    databaseURL: "https://meudinheiro-1bfcb.firebaseio.com",
+    projectId: "meudinheiro-1bfcb",
+    storageBucket: "meudinheiro-1bfcb.appspot.com",
+    messagingSenderId: "817742387581",
+    appId: "1:817742387581:web:049b86752f4673a84a1414",
+    measurementId: "G-EP09CZ9R8H"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  firebase.analytics();
+
+  export default firebase;
